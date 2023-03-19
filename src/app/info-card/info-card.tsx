d29d0e2267af1dc64d3e920f86f5eba3f62c00a8
wilson espina <wilson.espina@gmail.com>
2023-03-19 19:35:18 +1100

    feat: adds fluid fonts for info section

--- src/app/info-card/info-card.tsx
+++ src/app/info-card/info-card.tsx
@@ -1,5 +1,5 @@
 import cn from 'classnames';
-import wilsonIceCreamImage from '../../assets/wilson_ice_cream.jpg';
+import wilsonIceCreamImage from '../../assets/wilson_ice_cream.png';
 
 import styles from './info-card.module.scss';
 
@@ -12,10 +12,12 @@ export interface InfoCardProps {
 export function InfoCard({ name, job }: InfoCardProps) {
   return (
     <div className={cn(styles['info-card'], 'container')}>
-      <img src={wilsonIceCreamImage} alt="profile_picture" className={styles['image']}/>
       <div className={styles['info-container']}>
         <h1 className={styles['name']}>{name}</h1>
         <h2 className={styles['job']}>{job}</h2>
+      </div>
+      <div className={styles['image-container']}>
+        <img src={wilsonIceCreamImage} alt="profile_picture" className={styles['image']}/>
       </div>
     </div>
   );
