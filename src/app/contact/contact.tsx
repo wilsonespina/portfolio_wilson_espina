e74448aeac47a9a3f3dd3a0f83320bb8a3005188
wilson espina <wilson.espina@gmail.com>
2023-03-19 22:10:57 +1100

    feat: add font awesome package

--- src/app/contact/contact.tsx
+++ src/app/contact/contact.tsx
@@ -1,6 +1,4 @@
 import cn from 'classnames';
-// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-// import { solid, regular, brands, icon } from '@fortawesome/fontawesome-svg-core/import.macro';
 import styles from './contact.module.scss';
 
 /* eslint-disable-next-line */
@@ -10,8 +8,6 @@ export function Contact(props: ContactProps) {
   return (
     <div className={cn(`${styles['contact']} container`)}>
       <h2 className={'heading'}>Contact</h2>
-      <i></i>
-      {/* <FontAwesomeIcon icon={regular('coffee')} /> */}
     </div>
   );
 }
--- src/app/experience/experience.tsx
+++ src/app/experience/experience.tsx
@@ -1,4 +1,7 @@
 import cn from 'classnames';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faSquareJs, faNode } from '@fortawesome/free-brands-svg-icons'
+
 import styles from './experience.module.scss';
 
 /* eslint-disable-next-line */
@@ -8,6 +11,11 @@ export function Experience(props: ExperienceProps) {
   return (
     <div className={cn(`${styles['about']} container`)}>
       <h2 className="heading">Experience</h2>
+      <div className={cn(`${styles['icon-container']}`)}>
+        <FontAwesomeIcon icon={faSquareJs} className="icon fa-7x" bounce />
+        <FontAwesomeIcon icon={faNode} className="fa-7x" />
+
+      </div>
     </div>
   );
 }
