cd6e755b8c3ee2b275f5dfddcdc203aa969acfc4
wilson espina <wilson.espina@gmail.com>
2023-03-26 20:22:02 +1300

    feat: adds netlify deploy files

--- vite.config.ts
+++ vite.config.ts
@@ -1,45 +1,52 @@
 /// <reference types="vitest" />
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 import viteTsConfigPaths from 'vite-tsconfig-paths';
 import svgr from 'vite-plugin-svgr';
 
-export default defineConfig({
-  cacheDir: './node_modules/.vite/portfolio-wilson-espina',
+export default defineConfig(({ command, mode }) => {
+  const env = loadEnv(mode, process.cwd(), '')
 
-  server: {
-    port: 4200,
-    host: 'localhost',
-  },
-
-  preview: {
-    port: 4300,
-    host: 'localhost',
-  },
-
-  plugins: [
-    react(),
-    viteTsConfigPaths({
-      root: './',
-    }),
-    svgr(),
-  ],
-
-  // Uncomment this if you are using workers.
-  // worker: {
-  //  plugins: [
-  //    viteTsConfigPaths({
-  //      root: './',
-  //    }),
-  //  ],
-  // },
-
-  test: {
-    globals: true,
-    cache: {
-      dir: './node_modules/.vitest',
+  return ({
+    define: {
+      __APP_ENV__: env.APP_ENV,
     },
-    environment: 'jsdom',
-    include: ['src/**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx,svg}'],
-  },
+    cacheDir: './node_modules/.vite/portfolio-wilson-espina',
+
+    server: {
+      port: 4200,
+      host: 'localhost',
+    },
+
+    preview: {
+      port: 4300,
+      host: 'localhost',
+    },
+
+    plugins: [
+      react(),
+      viteTsConfigPaths({
+        root: './',
+      }),
+      svgr(),
+    ],
+
+    // Uncomment this if you are using workers.
+    // worker: {
+    //  plugins: [
+    //    viteTsConfigPaths({
+    //      root: './',
+    //    }),
+    //  ],
+    // },
+
+    test: {
+      globals: true,
+      cache: {
+        dir: './node_modules/.vitest',
+      },
+      environment: 'jsdom',
+      include: ['src/**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
+    }
+  })
 });
