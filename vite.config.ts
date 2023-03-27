5043b80afe15f3c05e6a505dccf9ffd21c411188
wilson espina <wilson.espina@gmail.com>
2023-03-28 07:59:11 +1300

    chore: remove unused function

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,5 @@
 /// <reference types="vitest" />
-import { defineConfig, loadEnv } from 'vite';
+import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import viteTsConfigPaths from 'vite-tsconfig-paths';
 import svgr from 'vite-plugin-svgr';
