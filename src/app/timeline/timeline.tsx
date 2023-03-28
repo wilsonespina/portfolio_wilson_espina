d710c68628cdc737b38ccb7083ecde2b25f8f410
wilson espina <wilson.espina@gmail.com>
2023-03-28 15:23:17 +1300

    feat: fixes overflow in timeline

--- src/app/timeline/timeline.tsx
+++ src/app/timeline/timeline.tsx
@@ -24,32 +24,30 @@ export function Timeline(props: TimelineProps) {
       animationOut="fadeOut"
       isVisible={true}>
       <div className={wrapper}>
-        <h1 className={styles['main-heading']}>Work Timeline</h1>
         <ul className={sessions}>
           <li className={item}>
             <div className={time}>March 2023</div>
-            <h2 className={heading}>Moved to Wellington, New Zealand</h2>
+            <h2 className={heading}><span role="img" aria-label="Plane emoji">✈️ </span>Moved to Wellington, New Zealand</h2>
             <p className={text}>Started a new adventure with my family</p>
           </li>
           <li className={item}>
             <div className={time}>March 2020</div>
-            <h2 className={heading}>Taught Javascript Development (part-time) course</h2>
+            <h2 className={heading}><span role="img" aria-label="Teacher emoji">👨‍🏫 </span>Taught Javascript Development (part-time) course</h2>
             <p className={text}>Mentored 100+ students</p>
           </li>
           <li className={item}>
-            <div className={time}>November 2018 2023</div>
-            <h2 className={heading}>Joined ITV</h2>
+            <div className={time}>November 2018</div>
+            <h2 className={heading}><span role="img" aria-label="Laptop emoji">💻 </span>Joined ITV</h2>
             <p className={text}>Built CTV app, web app, ITVX Streaming service</p>
           </li>
           <li className={item}>
             <div className={time}>February 2018</div>
-            <h2 className={heading}>Joined Limejump</h2>
+            <h2 className={heading}><span role="img" aria-label="Laptop emoji">💻 </span>Joined Limejump</h2>
             <p className={text}>Startup in the energy sector</p>
           </li>
           <li className={item}>
             <div className={time}>October 2017</div>
-            {/* <h2 className={heading}><span role="img" aria-label='Police siren emoji'>🚨</span>CAREER CHANGE<span role="img" aria-label='Police siren emoji'>🚨</span> - Web Development Immersive (WDI) @GA</h2> */}
-            <h2 className={heading}>Web Development Immersive (WDI) @GA</h2>
+            <h2 className={heading}><span role="img" aria-label="Student emoji">👨‍💻 </span>Web Development Immersive (WDI) @GA</h2>
             <p className={text}>Switched from Civil Engineering to a career in tech.</p>
           </li>
         </ul>
